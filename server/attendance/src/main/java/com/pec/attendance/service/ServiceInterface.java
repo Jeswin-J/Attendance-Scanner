f7c73616cdf5c9a1f1eccf47c83ec0b9198b51a1
Jeswin-J <jeswinjoseph164@gmail.com>
2024-12-13 15:36:02 +0530

    attendance record backend

--- server/attendance/src/main/java/com/pec/attendance/service/ServiceInterface.java
+++ server/attendance/src/main/java/com/pec/attendance/service/ServiceInterface.java
@@ -1,11 +1,17 @@
 package com.pec.attendance.service;
 
 import com.pec.attendance.model.Attendance;
+import com.pec.attendance.model.Student;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public interface ServiceInterface {
     Attendance markAttendance(String rollNumber);
 
     boolean hasAttendanceForToday(String rollNumber);
+
+    List<Student> attendanceRecord(LocalDate date);
 }
